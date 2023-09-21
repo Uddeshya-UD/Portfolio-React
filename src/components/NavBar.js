9becda57875e2315af170fd507e43d0ad5d39d8e
Uddeshya-UD <uddeshya31@gmail.com>
2023-09-21 15:43:36 +0530

    added profile links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,16 +9,16 @@ const NavBar = () => {
            <h1>Uddeshya verma</h1>
            <p>Systems Engineer<span> IN TCS</span></p>
           <ul className="sidebar-ul">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Projects</a></li>
-            <li><a href="#">Skills</a></li>
-            <li><a href="#">Contacts</a></li>
+            <li><a href="">Home</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Skills</a></li>
+            <li><a href="">Contacts</a></li>
           </ul>
                
           <ul>
-            <li><a href="#">Fb</a></li>
-            <li><a href="#">instagram</a></li>
-            <li><a href="#">linkedin</a></li>
+            <li><a href="https://www.facebook.com/profile.php?id=100011671078350">Fb</a></li>
+            <li><a href="https://www.instagram.com/uddeshya.music/">instagram</a></li>
+            <li><a href="https://www.linkedin.com/in/uddeshya-verma-185a49200/">linkedin</a></li>
           </ul>
 
           {/* <p>&copy; {new Date().getFullYear()} Uddeshya verma Portfolio. All rights reserved.</p> */}
